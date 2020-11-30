bd4e36db00e75d9fd097af2eba82a97e8eb1a32f
Charlliston Adrianni Ferreira <cadriann@everis.com>
2020-11-30 17:42:46 -0300

    actuator

--- agenda/src/main/java/br/com/adrianni/ch/agenda/AgendaApplication.java
+++ agenda/src/main/java/br/com/adrianni/ch/agenda/AgendaApplication.java
@@ -2,8 +2,10 @@ package br.com.adrianni.ch.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages={"br.com.adrianni.ch.agenda.controller"})
 public class AgendaApplication {
 
 	public static void main(String[] args) {
---
+++ agenda/src/main/java/br/com/adrianni/ch/agenda/config/SegurancaConfig.java
@@ -0,0 +1,25 @@
+package br.com.adrianni.ch.agenda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SegurancaConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().formLogin().permitAll().and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutUrl("/logout").and()
+				.httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("ADMIN");
+	}
+}
